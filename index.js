984d874cc2115fb40a1e0ca5b6f3433898274573
arpan gupta <111354376+arpangupta01@users.noreply.github.com>
2024-09-21 22:03:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let userinput=document.getElementById("date");
+userinput.max=new Date().toISOString().split("T")[0];
+let result=document.getElementById("result");
+let cal=document.getElementById("cal");
+
+
+
+
+
+
+
+
+function calculate(){
+  let birthdate=new Date(userinput.value);
+  let d1=birthdate.getDate();
+  let m1=birthdate.getMonth()+1;
+  let y1=birthdate.getFullYear();
+
+  let today =new Date();
+  let d2=today.getDate();
+  let m2=today.getMonth()+1;
+  let y2=today.getFullYear();
+
+  let d3,m3,y3;
+  y3=y2-y1;
+  if(m2>=m1){
+    m3=m2-m1;
+  }
+  else {
+    y3--;
+    m3=12 + m2-m1;
+  }
+
+  if(d2>=d1){
+    d3=d2-d1;
+
+  }
+  else {
+    m3--;
+    d3=get(y1,m1) +d2-d1;
+  }
+  if(m3<0){
+    m3=11;
+    y3--;
+  }
+  result.innerHTML=`You are ${y3}, ${m3} months and ${d3} daya old `;
+}
+
+
+function get(year,month){
+  return new Date(year,month,0).getDate();
+}
+
+cal.addEventListener("click",calculate);
